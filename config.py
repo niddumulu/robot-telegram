20743821fe47c26b146d1c11646f33e62d641072
niddumulu <36333121+niddumulu@users.noreply.github.com>
2019-07-23 10:12:07 +0700

    Penggantian anon

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ from telethon import TelegramClient, sync, events, utils
 api_id = 682610 
 api_hash = '030132b51d598e464419ccee7f20212d' 
 
-client = TelegramClient('tantowi', api_id, api_hash).start()
+client = TelegramClient('anon', api_id, api_hash).start()
