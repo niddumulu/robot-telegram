d55fdf8f6f0774c092e6b77a05f87c228d7b49a4
niddumulu <36333121+niddumulu@users.noreply.github.com>
2019-07-23 10:00:41 +0700

    file config

---
+++ config.py
@@ -0,0 +1,6 @@
+from telethon import TelegramClient, sync, events, utils
+
+api_id = 682610 
+api_hash = '030132b51d598e464419ccee7f20212d' 
+
+client = TelegramClient('tantowi', api_id, api_hash).start()
