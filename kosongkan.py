c64426edc93ba2482e6aeaa9e7f126ed04791d61
niddumulu <36333121+niddumulu@users.noreply.github.com>
2019-08-06 12:23:32 +0700

    Add files via upload

---
+++ kosongkan.py
@@ -0,0 +1,11 @@
+kos = ""
+teks = format(kos)
+file_nama = open("jualbeli.py","w")
+file_nama.write(teks)
+file_nama.close()
+
+kos = "HOLD"
+teks = format(kos)
+file_nama = open("kirimeksekusi.txt","w")
+file_nama.write(teks)
+file_nama.close()
