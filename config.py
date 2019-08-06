b05686de15078322449978eab507cea623749379
niddumulu <36333121+niddumulu@users.noreply.github.com>
2019-08-06 12:29:28 +0700

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-from telethon import TelegramClient, sync, events, utils
-
-api_id = 682610 
-api_hash = '030132b51d598e464419ccee7f20212d' 
-
-client = TelegramClient('anon', api_id, api_hash).start()
