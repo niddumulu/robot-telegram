698ed28d51e781a4214de7891ea9716fe93d3249
niddumulu <36333121+niddumulu@users.noreply.github.com>
2019-08-06 12:31:30 +0700

    Delete pesan.py

--- pesan.py
+++
@@ -1,5 +0,0 @@
-#Retrieving messages from a chat 
-from config import *
-
-for message in client.iter_messages('cryptoindobot', limit=1):
-          print(utils.get_display_name(message.sender), message.message) 
