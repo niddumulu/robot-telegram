ad160aaff626d7d26aa60fa10a09a96424307e12
niddumulu <36333121+niddumulu@users.noreply.github.com>
2019-08-06 12:15:25 +0700

    edit run

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-from config import *
 import time
 import os
 
@@ -11,21 +10,12 @@ while run.lower()=="dia":
     seconds = 0
     minutes = minutes+1
     if minutes == 1:
-      print ("saheu")
-      client.send_message('steemsbd', '/r steem sbd')
-    if minutes == 2:
-      for message in client.iter_messages('steemsbd', limit=1):
-         print(utils.get_display_name(message.sender), message.message)
-         pecah = (message.message)
-         simpan = pecah[78:84]
-         file_simpan = open("data.txt", "r+")
-         teks = file_simpan.readlines()
-         teks = format(simpan)+"\n"
-         file_simpan.write(teks)
-         file_simpan.close()
-      
-    if minutes == 15:
+      print ("sahal")
+      os.system("python robotgabung.py")
+    if minutes == 5:
+      print ("sahil")
       minutes = 0
+      
   #os.system('clear')
   seconds = (seconds+1)
   print (minutes," : ", seconds)
